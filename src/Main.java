747576827c78d7dd3a16eb99f9313aabb29c5ef3
disturbed-waffle <121725474+disturbed-waffle@users.noreply.github.com>
2023-01-12 16:06:28 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {//Main class contains main method
+
+	public static void main(String[] args) {
+            //constructs my custom JFrame when the project is run
+		var myFrame = new MyFrame();
+
+            
+	}
+
+}
